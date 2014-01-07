e838b41980f35726b40aabe69aa78b20b3fc33fc
EeshanChatterjee <eeshanchatterjee@gmail.com>
2014-01-07 12:31:22 +0530

    Update sentiment_analysis.R
    calling the polarity functions

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -76,6 +76,11 @@ confTable
 #run a binomial test for confidence interval of results
 binom.test(confTable[1,1] + confTable[2,2], nrow(results), p=0.5)
 
-#  chi sqr polarity test of the confusion table
+source("polarityFuncs.R")
 
+#  chi sqr polarity test of the confusion table
+chiSqrPV = calcChiSqrPV(as.vector(confTable))
+chiSqrPV
 # PMI-based polarity test for confTable
+PmiPV = calcPmiPV(as.vector(confTable))
+PmiPV
